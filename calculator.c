498009e9aa89d35d0e91d0662d31670f7a21ed8c
biruk <it.biruk.tesfaye@gmail.com>
2021-03-31 20:38:52 +0300

    added one function

--- calculator.c
+++ calculator.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
+
     puts("------------------");
     puts("Simple Calculator");
     puts("------------------");
+    
     int choose = 0;
-    while (choose != 5){
+
+    while (choose != 6){
+
         printf("\n\n");
-        puts(" 1. addition        2. multiplication \n 3. subtraction      4. division     5.quit");
+        puts(" 1. addition         2. multiplication \n\n 3. subtraction      4. division    \n\n 5.square_root        6.quit");
 
         printf("\n");
         printf("choose from the above: ");
@@ -25,11 +30,13 @@ int main(){
 
             int count = 0;
             int added = 0;
-            puts("enter the numbers that you want to add below");
+
+            puts("enter the numbers that you want to add below (hit enter after each input)");
+
             while (count < wanted){
                 int number = 0;
 
-                printf(">");
+                printf("> ");
                 scanf("%d", &number);
                 
                 added += number;
@@ -43,6 +50,7 @@ int main(){
         if (choose == 2){
 
             int wanted;
+
             printf("how many numbers do you want to mutiply: ");
             scanf("%d", &wanted);
 
@@ -51,7 +59,9 @@ int main(){
 
             int count = 0;
             int multiplied = 1;
+
             puts("\n enter the numbers that you want to multiply below");
+
             while (count < wanted){
                 int number = 0;
 
@@ -62,6 +72,7 @@ int main(){
                 count += 1;
 
             }
+
             printf("\nthe result is %d ", multiplied);
 
 
@@ -98,6 +109,19 @@ int main(){
             printf("\n %f / %f = %f ", num1, num2, div);
             continue;
 
+
+        }
+
+        if (choose == 5){
+            
+            int number;
+
+            printf("enter a number: ");
+            scanf("%d", &number);
+
+            double square_root = sqrt(number);
+
+            printf("\nthe square root of %d is %f", number, square_root);
 
         }
     }
